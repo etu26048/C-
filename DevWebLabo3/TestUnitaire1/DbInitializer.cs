a20fcd3094d03abf26280a13f8967139674470d2
etu26048 <lazrak.yassin@hotmail.Fr>
2016-09-29 14:45:39 +0200

    Dev Web Labo3

---
+++ DevWebLabo3/TestUnitaire1/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrairieDevWeb;
+using System.Data.Entity;
+
+namespace TestUnitaire1
+{
+    public class DbInitializer : DropCreateDatabaseAlways<CompanyContext>
+    {
+        protected override void Seed(CompanyContext context)
+        {
+            Customer customer = new Customer()
+            {
+                AccountBalance = 223.14,
+                AdressLine1 = "Rue des cerises",
+                AdressLine2 = "n25 boite 5",
+                City = "Namur",
+                Country = "Belgique",
+                Email = "Salut.toi@hotmail.Fr",
+                Id = 4,
+                Name = "Salut",
+                PostCode = "5000",
+                Remark ="Pas gentil"
+            };
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DevWebLabo3/TestUnitaire1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using LibrairieDevWeb;
+using System.Linq;
+
+namespace TestUnitaire1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            Database.SetInitializer(new DbInitializer());
+            using (CompanyContext context = GetContext())
+            {
+                context.Database.Initialize(true);
+            }
+        }
+
+        [TestMethod]
+        public void InsertionFonctionnelle()
+        {
+            using (var context = GetContext())  
+            {
+                Assert.AreEqual(1, context.Customers.ToList().Count);
+            }
+        }
+
+        private CompanyContext GetContext()
+        {
+            return new CompanyContext();
+        }
+
+       
+    }
+}
